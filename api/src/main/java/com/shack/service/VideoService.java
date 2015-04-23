ffb4d8c5583a7796ed42b0649b64b4b72bdfbabe
phoffmann <peter.hoffmann@magicinternet.de>
2015-04-23 19:59:06 +0200

    added new video model for toplist, code cleanup

--- api/src/main/java/com/shack/service/VideoService.java
+++ api/src/main/java/com/shack/service/VideoService.java
@@ -1,14 +1,11 @@
 package com.shack.service;
 
 import com.shack.model.Video;
+import com.shack.model.VideoVoted;
 import com.shack.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -18,22 +15,9 @@ import java.util.Random;
 public class VideoService {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
     private VideoRepository videoRepository;
 
     public Collection<Video> getVideos(final int rows) {
-/*        List<Video> results = jdbcTemplate.query(
-                "SELECT * from videos",
-                new RowMapper<Video>() {
-                    @Override
-                    public Video mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        return new Video(rs.getString("id"), rs.getString("title"),
-                                rs.getString("source"), rs.getString("image"));
-                    }
-                });
-*/
         List<Video> results = videoRepository.findAll();
         long seed = System.nanoTime();
         Collections.shuffle(results, new Random(seed));
@@ -43,8 +27,8 @@ public class VideoService {
         return results;
     }
 
-    public Collection<Video> getTopVideos(final int rows) {
-        List<Video> results = videoRepository.findTopVotes(rows);
+    public Collection<VideoVoted> getTopVideos(final int rows) {
+        List<VideoVoted> results = videoRepository.findTopVotes(rows);
         return results;
     }
 }
