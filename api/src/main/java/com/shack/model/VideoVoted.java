0f200c3f2511531df81f4b1720f2ffeda1861d2d
phoffmann <peter.hoffmann@magicinternet.de>
2015-04-23 18:21:56 +0200

    Added new model for voted videos

---
+++ api/src/main/java/com/shack/model/VideoVoted.java
@@ -0,0 +1,24 @@
+package com.shack.model;
+
+
+public class VideoVoted extends Video{
+
+    private int likes;
+    private int dislikes;
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(int dislikes) {
+        this.dislikes = dislikes;
+    }
+}
