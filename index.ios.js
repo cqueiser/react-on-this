43a838dd999cb1762cbf6a69ee8817fd36006c89
Markus Kleinhenz <markus.kleinhenz@prosiebensat1digital.de>
2015-04-23 17:35:44 +0200

    Moved Videoplayer in it's own component

--- index.ios.js
+++ index.ios.js
@@ -6,10 +6,6 @@
 
 var React = require('react-native');
 var VideoPlayer = require ('./VideoPlayer');
-var MOCKED_VIDEO_DATA = [
-  {"videoId":"MOVIE_11584253","formatName":"She Keeps Bees","clipTitle":"Is What It Is","source":"http://is.myvideo.de/movie23/95/11584253.mp4","image":"http://is.myvideo.de/movie23/95/thumbs/11584253_1.jpg"},
-  {"videoId":"MOVIE_11582975","formatName":"Pato Siebenhaar","clipTitle":"Fuld Af Løgn","source":"http://is.myvideo.de/movie18/db/11582975.mp4","image":"http://is.myvideo.de/movie18/db/thumbs/11582975_1.jpg"}];
-
 var {
   AppRegistry,
   StyleSheet,
@@ -24,35 +20,6 @@ var ReactOnThis = React.createClass({
       <VideoPlayer />
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    //backgroundColor: '#FFFFFF',
-  },
-  welcome: {
-    fontSize: 14,
-    textAlign: 'center',
-    color: '#FFFFFF',
-    margin: 5,
-  },
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  button: {
-    fontSize: 16,
-    margin: 5,
-    borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: '#d6d7da',
-    marginBottom: 5,
-  },
 });
 
 AppRegistry.registerComponent('ReactOnThis', () => ReactOnThis);
