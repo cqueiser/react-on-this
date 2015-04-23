556c192cb1b1ead5fb82f57d00c64884d40b5dd8
phoffmann <peter.hoffmann@magicinternet.de>
2015-04-23 17:37:05 +0200

    Added constructor in model

--- api/src/main/java/com/shack/model/Vote.java
+++ api/src/main/java/com/shack/model/Vote.java
@@ -21,6 +21,12 @@ public class Vote {
     @Column(name = "likes", nullable = false)
     private boolean likes;
 
+    public Vote(String user, String videoId, boolean likes) {
+        this.user = user;
+        this.videoId = videoId;
+        this.likes = likes;
+    }
+
     public String getUser() {
         return user;
     }
